ac375946cefcfad1001aa2130c4515f7aff9e3a3
Arhya Hafidz Hafidin <152181001+Arfidz12@users.noreply.github.com>
2025-12-15 20:39:19 +0700

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,22 +1,19 @@
-#pipe server
 from body_thread import BodyThread
 import time
 import global_vars
 from sys import exit
 
 if __name__ == "__main__":
-    # start body tracking thread
     body_thread = BodyThread()
     body_thread.start()
 
     try:
         i = input("Press Enter to stop...\n")
     except Exception:
-        # if input is interrupted, still proceed to shutdown
         pass
 
     print("Exiting…")
     global_vars.KILL_THREADS = True
-    # allow threads to exit gracefully
     time.sleep(0.5)
+
     exit()
